0d3324fcf5f2e217f7358f578fab24ad7353805e
avadhutap459 <avadhutap459@gmail.com>
2025-03-21 00:01:48 +0530

    added dto and models for shopping cart

---
+++ Mango.Service.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Service.ShoppingCartAPI.Models.Dto;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Service.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailId { get; set; }
+        public int CartHeaderId { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto Product { get; set; }
+        public int Count { get; set; }
+    }
+}
