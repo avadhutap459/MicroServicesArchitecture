f4e2fac82eacbe358ec82e325b4f42c2eb5505f2
avadhutap459 <avadhutap459@gmail.com>
2025-04-02 08:50:11 +0530

    added email and message bus project

---
+++ Mango.Service.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Service.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Service.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<EmailLogger> emaillogger { get; set; }
+    }
+}
